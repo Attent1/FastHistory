8bf4f6bf2494f5ec3d512195a5d1c2ddf01757f4
Attent1 <rafaeldsc22@outlook.com>
2024-04-21 13:54:32 -0300

    Criando endpoints de update, delete e getById para partida

--- src/main/java/br/com/fiap/fasthistory/controller/PartidaController.java
+++ src/main/java/br/com/fiap/fasthistory/controller/PartidaController.java
@@ -3,15 +3,23 @@ package br.com.fiap.fasthistory.controller;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND; 
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.fasthistory.model.Partida;
 import br.com.fiap.fasthistory.repository.PartidaRepository;
@@ -43,6 +51,37 @@ public class PartidaController {
         kda = (partida.getKill() + partida.getAssist()) / partida.getDeath();         
         partida.setKda(kda);
         return repository.save(partida);   
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Partida> get(@PathVariable Long id){                
+        return repository
+                    .findById(id)
+                    .map(ResponseEntity::ok) 
+                    .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void apagar(@PathVariable Long id){
+        log.info("Deletando partida com id: {}", id);
+        verificarSeExistePartida(id);
+        repository.deleteById(id);        
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(OK)
+    public Partida editar(@PathVariable Long id, @RequestBody Partida partida){
+        log.info("Atualizando partida com id: {}", id);
+        
+        verificarSeExistePartida(id);                  
+        partida.setId(id);    
+        return repository.save(partida);
+    }
+
+    private void verificarSeExistePartida(Long id) {
+        repository.findById(id)
+                  .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Partida não encontrada"));
     }
     
 }
